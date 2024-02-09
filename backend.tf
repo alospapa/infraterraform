aebc0965b6b10eee1e18c74786ecfaf5372d5491
Bernard <alospapa@yahoo.com>
2024-02-08 20:02:34 -0600

    change region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-bucket-040201"
     key = "main"
-    region = "us-east-1"
+    region = "us-west-1"
     dynamodb_table = "hashtekTerraformStatelock"
   }
 }
