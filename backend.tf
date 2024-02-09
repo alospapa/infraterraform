2510df843e29bc6b10247a746490c4b5f9fafeef
Bernard <alospapa@yahoo.com>
2024-02-08 21:31:07 -0600

    bucket name change

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket-040201"
+    bucket = "terraform-state-bucket-040202"
     key = "main"
     region = "us-west-1"
     dynamodb_table = "hashtekTerraformStatelock"
