fa4cf0342cecd6ff859427e452cea77714ea0235
Bernard <alospapa@yahoo.com>
2024-02-10 07:40:30 -0600

    ec2 changed

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-03f4878755434977f" 
+    default     = "ami-0ce2cb35386fc22e9" 
 }
 variable "versioning" {
     type        = bool
