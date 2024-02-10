845ae9a6a0f179d0397fa6078a6f8699f2b7c465
Bernard <alospapa@yahoo.com>
2024-02-10 07:52:11 -0600

    vpc

--- sonar.tf
+++ sonar.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "mySonarInstance" {
 
 # Create Elastic IP address for Sonar instance
 resource "aws_eip" "mySonarInstance" {
-  vpc      = "main"
+  vpc      = false
   instance = aws_instance.mySonarInstance.id
 tags= {
     Name = "sonar_elastic_ip"
